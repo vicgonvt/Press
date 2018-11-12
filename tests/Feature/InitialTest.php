7e047ba2d16de2201ae0dfaa54e174d4ed723608
Victor Gonzalez <vicgonvt@gmail.com>
2018-11-11 21:35:41 -0500

    fix composer.json file

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace vicgonvt\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
